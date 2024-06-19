51fcb2280a934b59ffcfbbce90cdda79f9a8194b
Leoska89 <83103793+Leoska89@users.noreply.github.com>
2024-06-19 08:57:52 -0500

    up

--- src/config/global.js
+++ src/config/global.js
@@ -129,6 +129,11 @@ export default {
         nombreRuta: 'referencias',
       },
       {
+        icono: 'fas fa-file-pdf',
+        titulo: 'Descargar PDF',
+        download: 'downloads/722103_CF03_DU.pdf',
+      },
+      {
         icono: 'fas fa-download',
         titulo: 'Descargar material',
         download: 'downloads/material.zip',
